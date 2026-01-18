06aa0c23a558d679966b8d0a6eb2c5d505a2e9cb
klangford <langford.scott.kaden@gmail.com>
2026-01-17 21:33:11 -0500

    modified TODO with stuff i'll do on the host vm tomorrow after we set up tailscale for me. i'll also install dbeaver

--- js/databaseconfig.js
+++ js/databaseconfig.js
@@ -1,3 +1,18 @@
 import 'dotenv/config';
-import pg from pg;
+import pg from 'pg';
 
+const {Pool}=pg;
+
+const pool = new Pool({
+    host: process.env.PGHOST,
+    post: Number(process.env.PGPORT),
+    database: process.env.PGDATABASE,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    ssl: false,
+    max: 10,
+    idleTimeoutMillis: 30_000,
+    connectionTimeoutMillis: 5_000,
+});
+
+export default pool;
